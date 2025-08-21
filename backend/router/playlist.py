e60e2531416447b8264256ee7e06d894bced7e6d
yuwei0603 <victor.aimhigh0603@gmail.com>
2025-08-21 17:38:15 +0800

    新增加入最愛至撥放清單模式

--- backend/router/playlist.py
+++ backend/router/playlist.py
@@ -2,19 +2,34 @@ from fastapi import APIRouter, Request, Depends, HTTPException
 from fastapi.responses import JSONResponse
 from fastapi import status
 
+import asyncio
+
 from utils.dependencies import check_session, get_user_voice_channel, get_playlist
 from utils.playing_list import GuildPlaylistsManager, Playlist
 from utils.download import download_audio
+from utils.db import get_songs
 
 router = APIRouter()
 
-@router.get("/add")
-async def add(request: Request, id: str, playlist= Depends(get_playlist)):
-    try:
+@router.post("/")
+async def add(request: Request, id: str, mode: str= 'song', session= Depends(check_session), playlist= Depends(get_playlist)):
+    async def download_and_add_song(id: str):
         url = f'https://www.youtube.com/watch?v={id}'
         song_info = await download_audio(url)
         playlist.add_song(song_info)
+        
+    try:
+        if mode == "song":
+            await download_and_add_song(id)
+        elif mode == "favorite":
+            name, songs = await get_songs(session['user_id'], id)
+            asyncio.gather(*[download_and_add_song(song['id']) for song in songs])
+        else:
+            raise HTTPException(status_code=400, detail="Invalid mode")
+    except HTTPException:
+        raise
     except Exception as e:
+        print(f"Error: {e}")
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             # detail=str(e)
@@ -22,8 +37,7 @@ async def add(request: Request, id: str, playlist= Depends(get_playlist)):
     return JSONResponse(
         status_code=status.HTTP_200_OK,
         content={
-            "message": "Song added to playlist",
-            "title": song_info["title"],
+            "message": "Added to playlist",
         }
     )
     
@@ -37,7 +51,11 @@ async def get(request: Request, playlist= Depends(get_playlist)):
             "thumbnail": song["thumbnail"],
             "channel": song["channel"],
         }, songs))
+    except HTTPException:
+        raise
+            
     except Exception as e:
+        print(f"Error: {e}")
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
         )
